185dd2aec378ecf6c36263dd9d2a3e3b112df12d
jurol <jurol@example.com>
2025-06-28 16:58:59 +0200

    Add decimals input

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
+const { log } = require("console")
+
 let nb1 = ''
 let nb2 = ''
 let operator = ''
+let isDecimalSeparator = false
 let isSecondNb = false
 let isSecondOperator = false
+let isResultDisplayed = false
 const displayElement = document.querySelector('.display')
+
+/**
+ * INPUTS
+ */
+let digits = document.querySelectorAll('.digit')
+let operators = document.querySelectorAll('.operator')
+let btn = document.querySelector('#btn').addEventListener('click', handleEvents)
+let clearButton = document.querySelector('.clear').addEventListener('click', handleEvents)
+let backspaceButton = document.querySelector('.backspace').addEventListener('click', handleEvents)
+Array.from(digits).map(item => item.addEventListener('click', handleEvents))
+Array.from(operators).map(item => item.addEventListener('click', handleEvents))
 
 function add(a, b) {
     return a + b
@@ -45,19 +60,80 @@ function operate(operator, numberA, numberB) {
 /**
  * Display the numbers, the result and the errors
  * 
- * @param {PointerEvent} e 
  * @returns 
  */
-function display(e) {
+function handleEvents(e) {
+    switch (e.target.dataset["type"]) {
 
-    if (e.target.dataset["key"] === "=") {
-        let nbA = parseInt(nb1)
-        let nbB = parseInt(nb2)
-        let result = displayElement.textContent = operate(operator, nbA, nbB)
-        clear()
-        return result
+        case "=":
+            displayResult()
+            break
+
+        case "digit":
+            displayNumber(e)
+            break
+
+        case "operator":
+            if (operator.length > 0) {
+                isSecondOperator = true
+                displayResult()
+            }
+            displayOperator(e)
+            break
+
+        case "backspace":
+            backspace(e)
+            break
+
+        case "clear":
+            clear()
+            break
     }
+}
 
+/**
+ * 
+ * @returns 
+ */
+function displayResult() {
+    try {
+        checkError()
+        let nbA = parseFloat(nb1)
+        let nbB = parseFloat(nb2)
+        nb1 = Math.round(operate(operator, nbA, nbB) * 100) / 100
+        nb2 = ""
+        if (!isSecondOperator) {
+            operator = ""
+            isResultDisplayed = true
+            isDecimalSeparator = false
+        }
+        isSecondNb = false
+        isSecondOperator = false
+        displayElement.textContent = nb1
+        document.getElementById(".").disabled = false
+    } catch (error) {
+        displayElement.textContent = error.message
+    }
+}
+
+
+/**
+ * 
+ */
+function displayNumber(e) {
+    if (e.target.textContent === ".") {
+        isDecimalSeparator = true
+        /**
+         * @type {HTMLButtonElement}
+         */
+        document.getElementById(".").disabled = true
+    }
+    console.log();
+    
+    if (isResultDisplayed) {
+        clear()
+        isResultDisplayed = false
+    }
     if (isSecondNb) {
         nb2 += e.target.textContent
         displayElement.textContent = nb2
@@ -68,40 +144,49 @@ function display(e) {
 }
 
 /**
- * Set the current number
- */
-function switchNumber() {
-    isSecondNb = !isSecondNb
-}
-
-/**
- * Set the operator
  * 
  * @param {PointerEvent} e 
  */
-function getOperator(e) {
-    if (operator.length > 0) {
-        
+function displayOperator(e) {
+    if (isResultDisplayed) {
+        isResultDisplayed = false
     }
     operator = e.target.textContent
-    switchNumber()
+    isSecondNb = true
+    document.getElementById(".").disabled = false
+}
+
+/**
+ * Si je n'ai pas entré le deuxieme nombre
+ * ou si j'essaye de diviser par zero
+ * 
+ * @returns {String || null}
+ */
+function checkError() {
+    if (nb2 === "") throw new Error("🤔")
+    if (operator === "/" && nb2 === "0")
+        throw new Error("Error")
 }
 
 /**
  * Reset the variables nb1, nb2, operator
  */
 function clear() {
+    displayElement.textContent = 0
     nb1 = ''
     nb2 = ''
     operator = ''
+    isSecondNb = false
+    isSecondOperator = false
+    isResultDisplayed = false
 }
 
-document.querySelector('.clear').addEventListener('click', () => {
-    displayElement.textContent = 0
-    clear()
-})
-let digits = document.querySelectorAll('.digit')
-let operators = document.querySelectorAll('.operator')
-let btn = document.querySelector('#btn').addEventListener('click', display)
-Array.from(digits).map(item => item.addEventListener('click', display))
-Array.from(operators).map(item => item.addEventListener('click', getOperator))
+function backspace(e) {
+    console.log("yolo");
+
+    nb1.split('').pop()
+    nb2.split('').pop()
+    displayNumber(e)
+}
+
+
